f8070a3b1218d3e1bd420189c58e35d9cf549acc
Douglas De Toni Machado <detoni.douglas@gmail.com>
2019-06-24 16:04:19 -0300

    adds caching

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"crypto/sha1"
+	"encoding/gob"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"time"
 )
+
+const cacheExpirationInSec = 10
 
 func main() {
 	var dst = flag.String("url", "", "destination URL to be proxied")
@@ -19,23 +28,77 @@ func main() {
 	}
 
 	proxy := NewProxy(dstURL)
-	proxy.Run()
+	proxy.Run(":8080")
 }
 
+//MemCache is a simple cache structure to hold the data and an expiration time
+type MemCache struct {
+	data      []byte
+	expiresAt time.Time
+}
+
+//NewMemCache returns a new MemCache with the default expiration time
+func NewMemCache(data []byte) *MemCache {
+	expireIn, _ := time.ParseDuration(fmt.Sprintf("%ds", cacheExpirationInSec))
+
+	return &MemCache{data, time.Now().Add(expireIn)}
+}
+
+//MyProxy is a simple reverse proxy supporting a minimal cache feature
 type MyProxy struct {
-	target *url.URL
+	target      *url.URL
+	memoryCache map[string]*MemCache
 }
 
+//NewProxy return an initialized MyProxy struct pointing to the target URL
 func NewProxy(target *url.URL) *MyProxy {
-	return &MyProxy{target}
+	return &MyProxy{target, make(map[string]*MemCache)}
 }
 
-func (p *MyProxy) Run() {
-	http.ListenAndServe(":8080", p)
+//Run starts the proxy server at the specified local address
+func (p *MyProxy) Run(addr string) {
+	http.ListenAndServe(addr, p)
 }
 
 func (p *MyProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("request received %v", r)
+
+	//Fetch response from the proxied URL
+	res, err := p.fetchResponse(r)
+	if err != nil {
+		log.Printf("error while trying to proxy\n request: %v\n response:%v\n%v", r, res, err)
+		return
+	}
+
+	//Write response to the client
+	copyHeader(w.Header(), res.Header)
+	w.WriteHeader(res.StatusCode)
+	_, err = io.Copy(w, res.Body)
+	if err != nil {
+		log.Printf("error copying response body: %v", err)
+	}
+	defer res.Body.Close()
+}
+
+func (p *MyProxy) fetchResponse(r *http.Request) (*http.Response, error) {
+	var response *http.Response
+
+	rHash := hashRequest(r)
+	//Check cache
+	if cacheData, exists := p.memoryCache[rHash]; exists {
+		if time.Now().Before(cacheData.expiresAt) {
+			log.Printf("cache hit %s", rHash)
+			respReader := bufio.NewReader(bytes.NewReader(cacheData.data))
+			response, err := http.ReadResponse(respReader, nil)
+			if err != nil {
+				log.Fatalf("error decoding cached response: %v", err)
+			}
+			response.Header.Set("X-Cached", "true")
+			return response, nil
+		}
+		log.Printf("cache expired %s", rHash)
+		delete(p.memoryCache, rHash)
+	}
 
 	//Creates the new request to the target proxy URL
 	pRequest := r.WithContext(r.Context())
@@ -50,24 +113,30 @@ func (p *MyProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Printf("sending request: %v", pRequest)
-	res, err := http.DefaultTransport.RoundTrip(pRequest)
-	if err != nil {
-		log.Printf("error while trying to proxy request: %v\n%v", pRequest, err)
-		return
+	response, err := http.DefaultTransport.RoundTrip(pRequest)
+	if err == nil {
+		//Add to cache
+		dumpedBody, err := httputil.DumpResponse(response, true)
+		p.memoryCache[rHash] = NewMemCache(dumpedBody)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	defer res.Body.Close()
+	return response, err
+}
 
-	copyHeader(w.Header(), res.Header)
-	w.WriteHeader(res.StatusCode)
-
-	buf := make([]byte, 16*1024)
-	_, err = io.CopyBuffer(w, res.Body, buf)
-	if err != nil {
-		log.Printf("error copying response body: %v", err)
-	}
+func hashRequest(r *http.Request) string {
+	//Trying to hash an http request without bothering
+	//with it's internal structure and order of attributes
+	var b bytes.Buffer
+	gob.NewEncoder(&b).Encode(r)
+	hashedBytes := sha1.New()
+	hashedBytes.Write(b.Bytes())
+	return fmt.Sprintf("%x", hashedBytes.Sum(nil))
 }
 
 func cloneHeader(h http.Header) http.Header {
+	//Straight from official go code
 	h2 := make(http.Header, len(h))
 	for k, vv := range h {
 		vv2 := make([]string, len(vv))
@@ -78,6 +147,7 @@ func cloneHeader(h http.Header) http.Header {
 }
 
 func copyHeader(dst, src http.Header) {
+	//Straight from official go code
 	for k, vv := range src {
 		for _, v := range vv {
 			dst.Add(k, v)
--- proxy_test.go
+++ proxy_test.go
@@ -7,7 +7,9 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"time"
 
+	"bou.ke/monkey"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,4 +33,71 @@ func TestSimpleRequest(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "Test Server Visited", w.Body.String())
+}
+
+func TestCachedRequest(t *testing.T) {
+	testServer := httptest.NewServer(&TestServer{})
+	defer testServer.Close()
+
+	u, _ := url.Parse(testServer.URL)
+	proxy := NewProxy(u)
+
+	//First request, added to cache
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", bytes.NewBufferString(""))
+	proxy.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "Test Server Visited", w.Body.String())
+	assert.Equal(t, "", w.Header().Get("X-Cached"))
+
+	//Cache hit
+	req, _ = http.NewRequest("GET", "/", bytes.NewBufferString(""))
+	w = httptest.NewRecorder()
+	proxy.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "Test Server Visited", w.Body.String())
+	assert.Equal(t, "true", w.Header().Get("X-Cached"))
+}
+
+func TestExpiredCachedRequest(t *testing.T) {
+	testServer := httptest.NewServer(&TestServer{})
+	defer testServer.Close()
+
+	u, _ := url.Parse(testServer.URL)
+	proxy := NewProxy(u)
+
+	//First request, added to cache
+	w := httptest.NewRecorder()
+	body := bytes.NewBufferString("")
+	req, _ := http.NewRequest("GET", "/", body)
+	proxy.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "Test Server Visited", w.Body.String())
+	assert.Equal(t, "", w.Header().Get("X-Cached"))
+
+	//Cache hit
+	req, _ = http.NewRequest("GET", "/", bytes.NewBufferString(""))
+	w = httptest.NewRecorder()
+	proxy.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "Test Server Visited", w.Body.String())
+	assert.Equal(t, "true", w.Header().Get("X-Cached"))
+
+	waitExpiration, _ := time.ParseDuration(fmt.Sprintf("%ds", cacheExpirationInSec+1))
+	futureTime := time.Now().Add(waitExpiration)
+	timePatch := monkey.Patch(time.Now, func() time.Time { return futureTime })
+	defer timePatch.Unpatch()
+
+	//Cache expired
+	req, _ = http.NewRequest("GET", "/", bytes.NewBufferString(""))
+	w = httptest.NewRecorder()
+	proxy.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "Test Server Visited", w.Body.String())
+	assert.Equal(t, "", w.Header().Get("X-Cached"))
 }
